b2b31b921ff3eda29946b7002ccd3691f815eaad
lindarosj <lrj9@hi.is>
2016-10-19 14:27:52 +0000

    bull

--- StartClass.java
+++ StartClass.java
@@ -3,5 +3,6 @@ public class StartClass {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 		System.out.println("Hallo Linda");
+		System.out.println("Hallo Halla");
 	}
 }
