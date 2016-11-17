db77589eb44d46c4d4c8a2776021c78f678acea3
Unnur Kristín <unnkbry@gmail.com>
2016-11-17 15:24:10 +0000

    Delete cloneForm.js

--- main/webapp/js/cloneForm.js
+++
@@ -1,7 +0,0 @@
-function CloneForm(formName) {
-    var formCount = document.forms.length;
-    var oForm = document.forms[formName];
-    var clone = oForm.cloneNode(true);
-    clone.name += "_" + formCount;
-    document.body.appendChild(clone);
-}​
