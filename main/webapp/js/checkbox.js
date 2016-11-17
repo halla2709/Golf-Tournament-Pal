bad2a60041065230f2f7076e6387780310139d02
Unnur Kristín <unnkbry@gmail.com>
2016-11-17 15:24:04 +0000

    Delete checkbox.js

--- main/webapp/js/checkbox.js
+++
@@ -1,5 +0,0 @@
-/*function checkBox(selectName) {
-var newdiv = document.createElement('div');
-newdiv.innerHTML = "Participant " + (counter + 1) + " <select class='w3-select' style='width:50%' id='numOfBrackets' name='2Inputs[]'>";
-document.getElementById(divName).appendChild(newdiv); }
-​
